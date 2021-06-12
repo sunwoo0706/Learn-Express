dd9a30cc0a24ef827247947e62f6e1599cc48237
Goolgae <sunwoo0706@icloud.com>
2021-06-13 00:45:54 +0900

    Update express version start server

--- index.js
+++
@@ -1,9 +0,0 @@
-const http = require("http");
-
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.write("Hello Server");
-    res.end();
-  })
-  .listen(3000);
