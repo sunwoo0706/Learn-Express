950846828f1c14c8a66efcd3c9cb93e8f53acacb
Goolgae <sunwoo0706@icloud.com>
2021-06-13 14:51:49 +0900

    Learn about router

---
+++ Routing/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const admin = require("./routes/admin");
+const contacts = require("./routes/contacts");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("express start");
+});
+
+app.use("/admin", admin);
+app.use("/contacts", contacts);
+
+app.listen(port, () => {
+  console.log("Express listening on port", port);
+});
